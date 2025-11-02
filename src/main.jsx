2bb8c2869425bf6444b6e85119048b60a6b55b62
Yogesh Sahu <cs20.yogesh.sahu@lcit.edu.in>
2025-11-02 16:46:29 +0530

    main.js file updated

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,11 @@ import { BrowserRouter } from 'react-router-dom'
 import StoreContextProvider from './context/StoreContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename='/Food-order-React'>
+  // This is for gh pages
+  // <BrowserRouter basename='/Food-order-React'>
+
+  // this is for vercel app
+  <BrowserRouter basename='/'>
     <StoreContextProvider>
       <App />
     </StoreContextProvider>
