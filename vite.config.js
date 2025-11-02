a6e565e8310b5f1372886c552d02ca1503e787d7
Yogesh Sahu <cs20.yogesh.sahu@lcit.edu.in>
2025-11-02 16:41:20 +0530

    placeorder css file updated

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+
+// This is for deploying on GitHub Pages
+// export default defineConfig({
+//   plugins: [react()],
+//   base:"/Food-order-React",
+// })
+
+// This is for deploying on other platforms like Netlify, Vercel, etc.
 export default defineConfig({
   plugins: [react()],
-  base:"/Food-order-React",
+  base:"./",
 })
